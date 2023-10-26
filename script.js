2c983ed4dd4d73da3cdef2126c78a4e184a834ad
kumarkanhai <kanhaikumarupvns@gmail.com>
2023-10-26 10:51:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+ let hrs = document.querySelector("#hrs");
+ let min = document.querySelector('#min');
+ let sec = document.querySelector('#sec');
+
+ setInterval(  ()=>{
+    let currentTime = new Date();
+
+hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+sec.innerHTML = currentTime.getSeconds();
+ },1000);
+
+ 
